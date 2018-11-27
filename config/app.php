42bb79ec9a3e445d33a5fbddd5299e1400e0ed4b
Manuel Strebel <strebel@novu.ch>
2018-11-27 23:16:02 +0100

    update laravel

--- config/app.php
+++ config/app.php
@@ -166,8 +166,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Laravel\Tinker\TinkerServiceProvider::class,
-        Barryvdh\Cors\ServiceProvider::class,
 
         /*
          * Application Service Providers...
